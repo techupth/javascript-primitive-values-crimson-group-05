b59ce0e9706e5a7ea0b139b3a315a6c283f73913
nongparanai <promlerd.p@gmail.com>
2023-11-23 15:14:06 +0700

    feat(exercise): add completed exercises

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let totalPrice = 27000;
+let discountPrice = (totalPrice * 25) / 100;
+let grandTotal = totalPrice - discountPrice;
+console.log(discountPrice);
+console.log(grandTotal);
