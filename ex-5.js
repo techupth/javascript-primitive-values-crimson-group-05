fe951c7886ff1d27411d52c43796f53dbce6e257
Waramet Hatjantuek <waramet.fl@gmail.com>
2023-11-23 14:25:29 +0700

    feat: add completed exercise

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let productDiscount = (totalPrice * 25) / 100;
+let totalPriceDiscount = totalPrice - productDiscount;
+
+console.log(totalPriceDiscount);
