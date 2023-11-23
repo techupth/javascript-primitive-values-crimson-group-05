ba72c7452a88303ee26a8a45a536f3fe667d4d2d
Mos amon <amonchanchai@gmail.com>
2023-11-23 16:09:18 +0700

    fix:ex1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,6 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPriceProduct = 27000
+let productDiscount = (totalPriceProduct*25)/100
+let totalPriceProductDiscount = totalPriceProduct-productDiscount
